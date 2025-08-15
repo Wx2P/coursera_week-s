0e309f47287329a5faab763bb599a6e510205427
Wx2P <sospls232323232333@gmail.com>
2025-08-15 21:06:10 +0300

    print a poem

---
+++ PrintAPoem/PrintAPoem.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Twinkle, twinkle, little star, \n How I wonder what you are. \n Up above the world so high, \nLike a diamond in the sky. ");
+	return 0;
+}
